5fd83a942724bb09a19e9bda0dd5c08f7d9316e0
jpolstre <jpolstre@gmail.com>
2019-08-31 21:35:55 -0400

    Updates

---
+++ precache-manifest.7171589c48d0ab15d67e3c44edba5fdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c134597dd69dffe2f1de579bb858fbb",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "187eb87a493b3aadf846",
+    "url": "/react-tutorial/static/css/main.b89bf1e9.chunk.css"
+  },
+  {
+    "revision": "d0ad48afd8f7971062a5",
+    "url": "/react-tutorial/static/js/2.06cce3f4.chunk.js"
+  },
+  {
+    "revision": "187eb87a493b3aadf846",
+    "url": "/react-tutorial/static/js/main.5f969796.chunk.js"
+  },
+  {
+    "revision": "be44a969ea6f00258bce",
+    "url": "/react-tutorial/static/js/runtime~main.4adc765e.js"
+  }
+]);
